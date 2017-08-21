00245140da34e960a9e2d969099395b6e3c70e7d
shedali <shedali@gmail.com>
2017-08-21 16:17:54 +0100

    init - scrape moneyhub for user net worth

---
+++ index.js
@@ -0,0 +1,31 @@
+var webdriverio = require('webdriverio'),
+    fs = require('fs'),
+    options = {
+    desiredCapabilities: {
+        browserName: 'chrome'
+    }
+};
+
+require('dotenv').config();
+
+webdriverio
+    .remote(options)
+    .init()
+    .url('https://client.moneyhub.co.uk')
+    .click('[data-aid=start-screen-signin]')
+    .setValue('[type=email]', process.env.email)
+    .setValue('[type=password]', process.env.password)
+    .click('[data-aid=signin-button]')
+    .waitForExist('[data-aid=AssetsAndLiabilitiesHero] span', 10000)
+    .getText('[data-aid=AssetsAndLiabilitiesHero] span').then(function(total) {
+        console.log('attempting to write total', total, 'utf8');
+        fs.writeFileSync("total.txt", total, function(err) {
+            console.log('in the write file sync thing');
+            if(err) {
+                return console.log(err);
+            }
+            
+        }); 
+        console.log('written to total.txt');
+    })
+    .end();
