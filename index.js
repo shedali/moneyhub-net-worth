d46c435312155c1dbdac24447d76d4647d64d9ab
shedali <shedali@gmail.com>
2018-10-06 17:00:41 +0100

    upgrade deps

--- index.js
+++ index.js
@@ -1,82 +1,79 @@
-var webdriverio = require('webdriverio'),
-    fs = require('fs'),
-    selenium = require('selenium-standalone'),
-    options = {
+var webdriverio = require("webdriverio"),
+  fs = require("fs"),
+  selenium = require("selenium-standalone"),
+  options = {
     desiredCapabilities: {
-        browserName: 'chrome'
+      browserName: "chrome"
     }
-};
+  };
 
-require('dotenv').config();
+require("dotenv").config();
 
+console.log("starting selenium standalone");
 
-console.log('starting selenium standalone');
-
-
-selenium.install({
-  version: '3.5.0',
-  baseURL: 'https://selenium-release.storage.googleapis.com',
-  drivers: {
-    chrome: {
-      version: '2.31',
-      arch: process.arch,
-      baseURL: 'https://chromedriver.storage.googleapis.com'
+selenium.install(
+  {
+    version: "3.12.0",
+    baseURL: "https://selenium-release.storage.googleapis.com",
+    drivers: {
+      chrome: {
+        version: "2.41",
+        arch: process.arch,
+        baseURL: "https://chromedriver.storage.googleapis.com"
+      },
+      firefox: {
+        version: "0.20.1",
+        arch: process.arch,
+        baseURL: "https://github.com/mozilla/geckodriver/releases/download"
+      }
     },
-    firefox: {
-      version: '0.18.0',
-      arch: process.arch,
-      baseURL: 'https://github.com/mozilla/geckodriver/releases/download'
+    logger: function(message) {},
+    progressCb: function(totalLength, progressLength, chunkLength) {
+      console.log(totalLength, progressLength, chunkLength);
     }
   },
-  logger: function(message) {
- 
-  },
-  progressCb: function(totalLength, progressLength, chunkLength) {
-        console.log(totalLength, progressLength, chunkLength)
-  }
-}, function(err, something){
-    if(err)console.log(err)
+  function(err, something) {
+    if (err) console.log(err);
 
-        selenium.start({
-  spawnOptions: {
-      stdio: 'inherit'
-  }
-}, function(err, selenium) {
+    selenium.start(
+      {
+        spawnOptions: {
+          stdio: "inherit"
+        }
+      },
+      function(err, selenium) {
+        if (err) {
+          console.log(err);
+          process.exit(1);
+        }
 
-    if(err){
-        console.log(err);
-        process.exit(1);
-    }
-
-  webdriverio
-    .remote(options)
-    .init()
-    .url('https://client.moneyhub.co.uk')
-    .click('[data-aid=start-screen-signin]')
-    .setValue('[type=email]', process.env.email)
-    .setValue('[type=password]', process.env.password)
-    .click('[data-aid=signin-button]')
-    .waitForExist('[data-aid=menu-accounts-assets]', 5000)
-    .click('[data-aid=menu-accounts-assets]')
-    .waitForExist('[data-aid=AssetsAndLiabilitiesHero] span', 10000)
-    .getText('[data-aid=AssetsAndLiabilitiesHero] span').then(function(total) {
-        console.log('attempting to write total', total, 'utf8');
-        fs.writeFileSync("total.txt", total, function(err) {
-            console.log('in the write file sync thing');
-            if(err) {
+        webdriverio
+          .remote(options)
+          .init()
+          .url("https://client.moneyhub.co.uk")
+          .click("[data-aid=start-screen-signin]")
+          .setValue("[type=email]", process.env.email)
+          .setValue("[type=password]", process.env.password)
+          .click("[data-aid=signin-button]")
+          .waitForExist("[data-aid=menu-accounts-assets]", 5000)
+          .click("[data-aid=menu-accounts-assets]")
+          .waitForExist("[data-aid=AssetsAndLiabilitiesHero] span", 10000)
+          .getText("[data-aid=AssetsAndLiabilitiesHero] span")
+          .then(function(total) {
+            console.log("attempting to write total", total, "utf8");
+            fs.writeFileSync("total.txt", total, function(err) {
+              console.log("in the write file sync thing");
+              if (err) {
                 return console.log(err);
-            }
-        }); 
-        console.log('written to total.txt');
-    })
-    .end()
-    .then(function(){
-        selenium.kill();
-    })
-});
-});
-
-
-
-
-
+              }
+            });
+            console.log("written to total.txt");
+          })
+          .end()
+          .then(function() {
+            selenium.kill();
+          });
+      }
+    );
+  }
+);
